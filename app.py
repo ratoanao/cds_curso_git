0afd7a93ab4547b249399f8a0ccbc9b8f796098e
Gustavo Vivi <luizgustavovivi@gmail.com>
2025-09-04 15:41:42 -0300

    Criado arquivo de extracao e ajustado arquivo principal

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import streamlit as st 
 
 def load_data():
-    return pd.read_csv('curso_git/projeto/data/processed/bikes_completed.csv')
+    return pd.read_csv('curso_git/projeto/data/bikes_completed.csv')
 
 def main():
     df = load_data()
